ae0e777c04e5280bbf5cd0acd43d1f2d6bb1cb5a
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-04-25 17:12:59 +0700

    Add Color Asset

---
+++ mozart/Views/StartGameView.swift
@@ -0,0 +1,18 @@
+//
+//  StartGameView.swift
+//  mozart
+//
+//  Created by Doni Pebruwantoro on 25/04/24.
+//
+
+import SwiftUI
+
+struct StartGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartGameView()
+}

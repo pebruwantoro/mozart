23ca4ad7d8fbf9c0bcce70a70123fc04c758ca52
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-04-25 10:22:14 +0700

    Initial Commit

---
+++ mozart/mozartApp.swift
@@ -0,0 +1,17 @@
+//
+//  mozartApp.swift
+//  mozart
+//
+//  Created by Doni Pebruwantoro on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct mozartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
